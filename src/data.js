861afaad37cbeb41192d723c3169541cdebd1eb6
Serhat Polat <SerhatLiving@hotmail.com>
2021-07-11 21:07:48 +0300

    Image fetching way is changed
    Before this change product images was coming from internet, now comes from local.

--- src/data.js
+++ src/data.js
@@ -1,24 +1,26 @@
+import guitarphoto from "./productImages/guitarphoto.jpg";
+import headsetphoto from "./productImages/headsetphoto.jpg";
+import phonephoto from "./productImages/phonephoto.jpg";
+
 const productData = {
   products: [
     {
       id: "1",
       name: "Guitar",
       price: 1199,
-      image: "http://images.richtonemusic.co.uk/product/JACK291-0238-503b.jpg",
+      image: guitarphoto,
     },
     {
       id: "2",
       name: "Headset",
       price: 49,
-      image:
-        "https://www.overgameweb.com/uploads/ps5-pulse-3d-wireless-headset-3640-lf3d177w.jpg",
+      image: headsetphoto,
     },
     {
       id: "3",
       name: "Phone",
       price: 1399,
-      image:
-        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/iphone12-gorseli-110624_large.jpg",
+      image: phonephoto,
     },
   ],
 };
